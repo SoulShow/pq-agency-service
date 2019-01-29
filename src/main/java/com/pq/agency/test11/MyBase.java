730a966fd6eb48f571cf75b3f1d7481507af2e25
liutao <zhitao0902@163.com>
2019-01-29 10:12:31 +0800

    update

--- src/main/java/com/pq/agency/test11/MyBase.java
+++ src/main/java/com/pq/agency/test11/MyBase.java
@@ -1,18 +1,18 @@
 package com.pq.agency.test11;
 
-public class MyBase extends Base{
+public class MyBase{
 
-    MyBase(){
-        System.out.println("my base class construtor:");
-        add(2);
-
-    }
-
-    @Override
-    public void add(int v) {
-        System.out.println("my base add");
-        i+=v*2;
-        System.out.println("my base class add method:"+i);
-
-    }
+//    MyBase(){
+//        System.out.println("my base class construtor:");
+//        add(2);
+//
+//    }
+//
+//    @Override
+//    public void add(int v) {
+//        System.out.println("my base add");
+//        i+=v*2;
+//        System.out.println("my base class add method:"+i);
+//
+//    }
 }
--- src/main/java/com/pq/agency/test11/Test.java
+++ src/main/java/com/pq/agency/test11/Test.java
@@ -2,10 +2,8 @@ package com.pq.agency.test11;
 
 public class Test{
     public static void main(String[] arg){
-        go(new MyBase());
+//        go(new MyBase());
     }
-    public static void go(Base b){
-        b.add(8);
-    }
+
 
 }
