555ff4b10f38e4ea2d6c434dfba15df403aca78e
liutao <zhitao0902@163.com>
2019-01-29 10:13:49 +0800

    update

--- src/main/java/com/pq/agency/test11/Test.java
+++ src/main/java/com/pq/agency/test11/Test.java
@@ -1,9 +1,9 @@
 package com.pq.agency.test11;
 
 public class Test{
-    public static void main(String[] arg){
-//        go(new MyBase());
-    }
+//    public static void main(String[] arg){
+////        go(new MyBase());
+//    }
 
 
 }
