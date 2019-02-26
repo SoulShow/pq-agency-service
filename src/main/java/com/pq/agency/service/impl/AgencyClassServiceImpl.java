9bb3b0b109d9977edfabe0fd998078141819982b
liutao <zhitao0902@163.com>
2019-02-26 17:49:47 +0800

    udpate

--- src/main/java/com/pq/agency/service/impl/AgencyClassServiceImpl.java
+++ src/main/java/com/pq/agency/service/impl/AgencyClassServiceImpl.java
@@ -574,7 +574,7 @@ public class AgencyClassServiceImpl implements AgencyClassService {
         userNoticeFileCollection.setFile(noticeFileCollectionForm.getFileUrl());
         userNoticeFileCollection.setFileName(noticeFileCollectionForm.getFileName());
         if(noticeFileCollectionForm.getFileSize()!=null){
-            userNoticeFileCollection.setFileSize(Double.valueOf(noticeFileCollectionForm.getFileSize())/1024+"KB");
+            userNoticeFileCollection.setFileSize((Integer.valueOf(noticeFileCollectionForm.getFileSize())/1024)+"KB");
         }
         userNoticeFileCollection.setUserId(noticeFileCollectionForm.getUserId());
         userNoticeFileCollection.setStudentId(noticeFileCollectionForm.getStudentId()==null?
